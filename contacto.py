4a2871d43422cfe39edf2d53293b29987f6b1ce0
matias <matiasfigueiras@hotmail.com>
2019-10-12 14:38:29 -0300

    clase contacto creada con el metodo inicializdor probado
    Signed-off-by: matias <matiasfigueiras@hotmail.com>

---
+++ contacto.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import datetime
+
+ultimo_id=1
+
+class Contacto:
+    '''Contiene una lista de clientes self.clientes, permite agregar o quitar'''
+    def __init__(self,nombre,apellido,domicilio,telefono,mail):
+        '''crea un objeto contacto '''
+        
+        
+        self.nombre=nombre
+        self.apellido=apellido
+        self.domicilio=domicilio
+        self.telefono=telefono
+        self.mail=mail
+        global ultimo_id
+        self.id = ultimo_id
+        ultimo_id+= 1
+    
+
+    def buscar(self,id):
+        
+        pass
+    
