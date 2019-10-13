63539e856617a100165c220643b2be603e6468a5
Alexia <alelugo318@gmail.com>
2019-10-13 18:45:42 -0300

    sumados los metodos:agregar cliente, eliminar, buscar y buscar por id

--- clientes.py
+++ clientes.py
@@ -15,4 +15,28 @@ class Clientes:
         self.clientes.append(cliente)
         return cliente
 
+    def eliminar_cliente(self,id_cliente):
+        cliente= self.buscar_cliente(id_cliente)
+        if cliente :
+            self.clientes.remove(cliente)
+                
+        else:
+            return "cliente no encontrado"
+            
+    def buscar_cliente_por_id(self,id_cliente):
+        ''' busca el cliente con el id dado y lo retorna'''
+        for cliente in self.clientes:
+            if str(cliente.id) == str(id_cliente):
+                return cliente
+            else:
+                return False
+    def buscar_cliente(self,filtro):
+        '''recibe un filtro y hace la busqueda sobre nombre'''
+        lista=[]
+        for cliente in self.clientes:
+            if str(cliente.nombre)== str(filtro):
+                lista.append(cliente)
+        return lista
+    
+
     
