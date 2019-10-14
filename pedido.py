4df92fa12150aadbdeac082214469b79cc097a17
matias <matiasfigueiras@hotmail.com>
2019-10-13 23:16:07 -0300

    clase pedido funcionando

---
+++ pedido.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+from datetime import datetime
+from contacto import Contacto
+from clientes import Clientes
+
+ultimoid=0
+class Pedido:
+    '''genera el pedido contodos sus campos'''
+    def __init__(self, id_cliente, descripcion, etiquetas=' ', fecha_prev=' ', fecha_entrega=' ',
+                 id_estado=' ',precio=' ',pagado=' '):
+        self.descripcion=descripcion
+        self.etiquetas=etiquetas
+        self.fecha_recep=datetime.today()
+        self.fecha_prev=fecha_prev
+        self.fecha_entrega=fecha_entrega
+        self.id_estado=id_estado
+        self.id_cliente=id_cliente
+        self.precio=precio
+        self.pagado=pagado
+        global ultimoid
+        ultimoid=ultimoid+1
+        self.id_pedido=ultimoid
+       
