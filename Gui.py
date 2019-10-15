8fa6ecb863ca5fcadf512f829268b27d2e4f0dd2
Alexia <alelugo318@gmail.com>
2019-10-15 15:59:04 -0300

    Agregada interfaz: agregar pedido,cerrar pedido, buscar pedido y guardar

--- Gui.py
+++ Gui.py
@@ -51,6 +51,86 @@ class Gui:
         botonSalir = tkinter.Button(self.raiz_sistema, text = "Salir",
                     command = self.raiz_sistema.destroy)
         botonSalir.grid(row=3, column=1)
+        
+    def agregar_pedido(self):
+        '''ventana para ingresar datos de pedido'''
+        self.raiz_nuevopedido=tkinter.Toplevel()
+        self.raiz_nuevopedido.title("agregar pedido")
+        self.raiz_nuevopedido.geometry("400x250+0+0")
+
+        self.caja_descripcion=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_descripcion.grid(row=0,column=1)
+        self.caja_etiquetas=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_etiquetas.grid(row=1,column=1)
+        self.caja_fecha_prev=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_fecha_prev.grid(row=2,column=1)
+        self.caja_precio=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_precio.grid(row=3,column=1)
+        self.caja_pagado=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_pagado.grid(row=4,column=1)
+        self.caja_idcliente=tkinter.Entry(self.raiz_nuevopedido)
+        self.caja_idcliente.grid(row=6,column=1)
+
+        tkinter.Label(self.raiz_nuevopedido,text="Descripcion").grid(row=0,column=0)
+        tkinter.Label(self.raiz_nuevopedido,text="Etiquetas").grid(row=1,column=0)
+        tkinter.Label(self.raiz_nuevopedido,text="Fecha previstas").grid(row=2,column=0)
+        tkinter.Label(self.raiz_nuevopedido,text="Precio").grid(row=3,column=0)
+        tkinter.Label(self.raiz_nuevopedido,text="Pagado").grid(row=4,column=0)
+
+        boton_salir=tkinter.Button(self.raiz_nuevopedido,text="Salir",
+                                   command= self.raiz_nuevopedido.destroy).grid(row=5,column=1)
+        boton_guardar=tkinter.Button(self.raiz_nuevopedido,text="Guardar",
+                                   command= self.guardar_pedido).grid(row=5,column=0)
+        boton_clientes=tkinter.Button(self.raiz_nuevopedido,text="Cliente",
+                                   command= self.insertar_cliente).grid(row=5,column=2)
+        
+        self.raiz_nuevopedido.grab_set()
+        self.raiz_sistema.wait_window(self.raiz_nuevopedido)
+
+
+       
+        
+    
+    def cerrar_pedido(self):
+        '''marca el pedido como entregado y lo muestra'''
+        i = self.treeview.selection()
+        id = self.treeview.item(i)['text']
+        print (id)
+        clt=self.pedidos.entregar_pedido(id)
+
+         # Vaciar el treeview
+        for i in self.treeview.get_children():
+            self.treeview.delete(i)
+            
+        self.treeview.insert("",tkinter.END, text=clt.id_pedido,
+                values=(clt.descripcion, clt.etiquetas, clt.fecha_prev, clt.id_estado, clt.pagado ))
+        
+
+
+    def buscar_pedido(self):
+        '''captura el contenido de la caja buscar y se la envia a pedido-buscar peddido,busca en descripcion y etiquetas'''
+        filtro=self.cajaBuscar_pedido.get()
+        resultado=self.pedidos.buscar_pedido(filtro)
+
+         # Vaciar el treeview
+        for i in self.treeview.get_children():
+            self.treeview.delete(i)
+        for clt in resultado:    
+            self.treeview.insert("",tkinter.END, text=clt.id_pedido,
+                    values=(clt.descripcion, clt.etiquetas, clt.fecha_prev, clt.id_estado, clt.pagado ))
+        
+            
+
+    def guardar_pedido(self):
+        '''guarda los datos ingresados en la ventana de nuevo pedido'''
+        clt= self.pedidos.nuevo_pedido(self.caja_idcliente.get(),self.caja_descripcion.get(), self.caja_etiquetas.get(), self.caja_fecha_prev.get(), self.caja_precio.get(), self.caja_pagado.get())
+        self.raiz_nuevopedido.destroy()
+
+
+        
+
+        self.treeview.insert("",tkinter.END, text=clt.id_pedido,
+                values=(clt.descripcion, clt.etiquetas, clt.fecha_prev, clt.precio, clt.pagado ))
 
 
 
