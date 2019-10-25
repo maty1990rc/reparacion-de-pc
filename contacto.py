6cfcf3e38440faf4d6f7ec1514ca9b16dc2dd80f
lularocca20 <lucianalarocca20@gmail.com>
2019-10-25 16:35:39 -0300

    clase contacto

--- contacto.py
+++ contacto.py
@@ -5,8 +5,8 @@ ultimo_id=1
 
 class Contacto:
     '''Contiene una lista de clientes self.clientes, permite agregar o quitar'''
-    def __init__(self,nombre,apellido,domicilio,telefono,mail):
-        '''crea un objeto contacto con todos los datos del contacto'''
+    def __init__(self,nombre,apellido,domicilio,telefono,mail,id=None):
+        '''crea un objeto contacto '''
         
         
         self.nombre=nombre
@@ -15,7 +15,7 @@ class Contacto:
         self.telefono=telefono
         self.mail=mail
         global ultimo_id
-        self.id = ultimo_id
+        self.id = id
         ultimo_id+= 1
     
 
